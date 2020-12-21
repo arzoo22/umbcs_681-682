64e31a2566eb1261c62c1b5f387663c20ee4114e
arzoo22 <71057656+arzoo22@users.noreply.github.com>
2020-12-21 21:22:40 +0530

    Add files via upload

---
+++ HWS/HW16/src/edu/umb/cs681/hw16/Observer.java
@@ -0,0 +1,5 @@
+package edu.umb.cs681.hw16;
+
+public interface Observer {
+	public void update(Observable o, Object arg);
+}
---
+++ HWS/HW16/src/edu/umb/cs681/hw16/TeslaStockEvent.java
@@ -0,0 +1,12 @@
+package edu.umb.cs681.hw16;
+
+public class TeslaStockEvent {
+	Float quote;
+	public TeslaStockEvent(Float quote) {
+		this.quote = quote;
+	}
+	public Float getQuote() {
+		return this.quote;
+	}
+}
+
